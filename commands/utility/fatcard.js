93b2f8dd0d55ba4a2c7cb68aa49718e68fc301d9
ruck <leekahwin@hotmail.com>
2026-01-31 17:33:57 +0800

    removed text for fatcard and smallcard

--- commands/utility/fatcard.js
+++ commands/utility/fatcard.js
@@ -36,7 +36,6 @@ module.exports = {
         const imageUrl = `https://deckofcardsapi.com/static/img/${rank.code}${suit.code}.png`;
 
         const embed = new EmbedBuilder()
-            .setTitle('🃏 You drew a card')
             .setImage(imageUrl);
 
         await interaction.reply({ embeds: [embed] });
